84b6cdbcf2a921fa83bd4fcef4fa46f54b13f29d
Ubuntu <ubuntu@ingest.hagley.dgicloud.com>
2018-03-12 14:01:59 +0000

    new branch

---
+++ templates/islandora-html5-video-stack.tpl.php
@@ -0,0 +1,40 @@
+<script src="sites/all/modules/responsive_menus/styles/meanMenu/jquery.meanmenu.min.js"></script>
+<script src="sites/all/modules/responsive_menus/styles/meanMenu/responsive_menus_mean_menu.js?ocw37s"></script>
+<div class="row">
+  <div class="col-sm-12 col-md-12">
+    <div data-transcripts-role="video" data-transcripts-id="<?php print $params['trid']; ?>">
+      <div align="center">
+        <video class="video-js vjs-default-skin embed-responsive-item" controls poster="<?php print $params['tn']; ?>" preload="auto"  width="100%" height="360" data-setup="{}">
+          <source src="<?php print $params['url']; ?>" type="<?php print $params['mime']; ?>"/>
+          <?php if (isset($params['tracks']) && $params['enable_transcript_display']): ?>
+            <?php foreach ($params['tracks'] as $key => $track): ?>
+              <track id="track<?php print $key; ?>" src="<?php print $track['source_url']; ?>" srclang="<?php print $track['lang_code']; ?>"
+                     kind="captions" label="<?php print $track['lang_code']; ?>" default>
+            <?php endforeach; ?>
+          <?php endif; ?>
+        </video>
+      </div>
+    </div>
+  </div>
+  <div id="transcript-tabs" class="col-sm-12 col-md-12">
+    <ul id="tabs-list">
+      <li><a href="#transcript-tab">Index</a></li>
+      <?php if (array_key_exists('annotation_content', $params)): ?>
+        <li><a href="#annotation-tab">Annotation</a></li>
+      <?php endif; ?>
+    </ul>
+    <div id="transcript-tab">
+      <?php if (array_key_exists('transcript_content', $params)) {
+        print $params['transcript_content'];
+      } else {
+        print "No Content available.";
+      }
+      ?>
+    </div>
+    <?php if (array_key_exists('annotation_content', $params)): ?>
+      <div id="annotation-tab">
+        <?php print $params['annotation_content']; ?>
+      </div>
+    <?php endif; ?>
+  </div>
+</div>
---
+++ templates/islandora-oralhistories.tpl.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * @file
+ * This is the template file for the object page for oralhistories
+ *
+ * Available variables:
+ * - $islandora_object: The Islandora object rendered in this template file
+ * - $islandora_dublin_core: The DC datastream object
+ * - $dc_array: The DC datastream object values as a sanitized array. This
+ *   includes label, value and class name.
+ * - $islandora_object_label: The sanitized object label.
+ * - $parent_collections: An array containing parent collection(s) info.
+ *   Includes collection object, label, url and rendered link.
+ *
+ * @see template_preprocess_islandora_oralhistories()
+ * @see theme_islandora_oralhistories()
+ */
+?>
+
+<div class="islandora-oralhistories-object islandora" vocab="http://schema.org/" prefix="dcterms: http://purl.org/dc/terms/" typeof="VideoObject">
+  <div class="islandora-oralhistories-content-wrapper  clearfix">
+    <?php if ($islandora_content['viewer']): ?>
+      <div class="islandora-oralhistories-content">
+        <?php print $islandora_content['viewer']; ?>
+      </div>
+    <?php endif; ?>
+  </div>
+  <div class="islandora-oralhistories-metadata">
+    <?php print $description; ?>
+    <?php if ($parent_collections): ?>
+      <div>
+        <h2><?php print t('In collections'); ?></h2>
+        <ul>
+          <?php foreach ($parent_collections as $collection): ?>
+        <li><?php print l($collection->label, "islandora/object/{$collection->id}"); ?></li>
+          <?php endforeach; ?>
+        </ul>
+      </div>
+    <?php endif; ?>
+    <?php print $metadata; ?>
+  </div>
+</div>
+<link type="text/css" rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.6.4/css/bootstrap-select.min.css" media="all" />
+<script type = "text/javascript" language = "javascript">
+        jQuery(window).bind("load", function() {
+            jQuery("span.text:contains('Transcript')").parents("li.selected").removeClass("selected");
+            jQuery("div[data-tier='or_transcriptFull']").css('display','none');
+         });
+
+      </script> 
+
